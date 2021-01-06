00eadf9bc2bb7fcb09eab3d077fba0762fefc90c
KhayaZikhali <70526289+KhayaZikhali@users.noreply.github.com>
2021-01-06 23:58:31 +0100

    improved the button layout
    added an state update function
    added a fucntion to update thed display on button click

--- src/calc.js
+++ src/calc.js
@@ -6,38 +6,45 @@ class Main extends React.Component {
     constructor(props) {
       super(props);
       this.state = {
-        instrument: 0
+       display  : 1
       };
       this.lings = this.lings.bind(this)
+      this.cancel = this.cancel.bind(this)
     }
+
  // function to alter the state
-    lings(){
+    lings(num){
         this.setState (state => { 
-            return {instrument : 1 } })
+            return {display : this.state.display + 3 } })
+    }
+
+    cancel(){
+        this.setState (state =>{ 
+            return {display : 0 } })
     }
 
     render() {
     return (
         <div className="main">
-          <div className="display">{this.state.instrument}</div>  
-            <button className="btn btn-lg btn-danger"  id="cancel">AC</button>
-              <button className="btn btn-lg btn-secondary" id="divide">/</button>
-              <button className="btn btn-lg btn-secondary" id="add">+</button>
-              <button className="btn btn-lg btn-secondary" id="subtract">-</button>
-              <button className="btn btn-lg btn-secondary" id="multiply">*</button>
-            <button className="btn btn-primary btn-click" id='nine'>9</button>
-            <button className="btn btn-primary btn-click" id='eight'>8</button>
-            <button className="btn btn-primary btn-click" id='seven'>7</button>
-            <button className="btn btn-primary btn-click" id='six'>6</button>
-            <button className="btn btn-primary btn-click" id='five'>5</button>
-            <button className="btn btn-primary btn-click" id='four'>4</button>
-            <button className="btn btn-primary btn-click" id='three'>3</button>
-            <button className="btn btn-primary btn-click" id='two'>2</button>
-            <button className="btn btn-primary btn-click" id='one'>1</button>
-            <button className="btn btn-primary btn-click" id='point'>.</button>
-            <button className="btn btn-primary btn-click" id='equal'>=</button>
-            
-            <button className="btn btn-primary btn-click" id="zero">0</button>
+            <div className="display">{this.state.display}</div>  
+            <button className="btn btn-lg btn-danger" id="cancel" onClick={this.cancel}>AC</button>
+            <button className="btn btn-lg btn-secondary" id="divide" >/</button>
+            <button className="btn btn-lg btn-secondary" id="add" >+</button>
+            <button className="btn btn-lg btn-secondary" id="subtract">-</button>
+            <button className="btn btn-lg btn-secondary" id="multiply">*</button>
+            {/* add shadow to buttons */}
+            <button className="btn button-secondary btn-lg" id='equal' onClick={this.lings}>=</button>
+            <button className="btn btn-primary btn-click" id='nine' value={9} onClick={this.lings}>9</button>
+            <button className="btn btn-primary btn-click" id='eight' onClick={this.lings}>8</button>
+            <button className="btn btn-primary btn-click" id='seven' onClick={this.lings}>7</button>
+            <button className="btn btn-primary btn-click" id='six' onClick={this.lings}>6</button>
+            <button className="btn btn-primary btn-click" id='five' onClick={this.lings}>5</button>
+            <button className="btn btn-primary btn-click" id='four' onClick={this.lings}>4</button>
+            <button className="btn btn-primary btn-click" id='three' onClick={this.lings}>3</button>
+            <button className="btn btn-primary btn-click" id='two' onClick={this.lings}>2</button>
+            <button className="btn btn-primary btn-click" id='one' onClick={this.lings}>1</button>
+            <button className="btn btn-primary btn-click" id='point' onClick={this.lings}>.</button>
+            <button className="btn btn-primary btn-click" id="zero" onClick={this.lings}>0</button>
         </div>
     )
  }
