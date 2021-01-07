0540319616c20ce45c965c556b4d332e6594943e
KhayaZikhali <70526289+KhayaZikhali@users.noreply.github.com>
2021-01-07 20:34:35 +0100

    added handleClick to the buttons

--- src/calc.js
+++ src/calc.js
@@ -2,23 +2,37 @@ import React from 'react'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+// find a way to display the numbers that a re clicked on to the display
+
 class Main extends React.Component {
     constructor(props) {
       super(props);
       this.state = {
-       display  : 1
+       display  : " "
       };
-      this.lings = this.lings.bind(this)
-      this.cancel = this.cancel.bind(this)
+      this.reset = this.reset.bind(this)
+      this.handleClick = this.handleClick.bind(this)
+      this.equal = this.equal.bind(this)
     }
 
  // function to alter the state
-    lings(num){
-        this.setState (state => { 
-            return {display : this.state.display + 3 } })
+ //  add event listeners  
+     
+    handleClick = (value) => {
+        this.setState({ display : this.state.display + value})
+      }
+    
+    equal(){
+            var result = eval(document.getElementById('display').value);
+            document.getElementById('display').value = result;
+            var x = document.getElementById('display').value    
+            console.log(eval(this.state.display))
+            this.setState(state=>{
+                return{ display : eval(this.state.display) }
+            })
     }
 
-    cancel(){
+    reset(){
         this.setState (state =>{ 
             return {display : 0 } })
     }
@@ -26,25 +40,25 @@ class Main extends React.Component {
     render() {
     return (
         <div className="main">
-            <div className="display">{this.state.display}</div>  
-            <button className="btn btn-lg btn-danger" id="cancel" onClick={this.cancel}>AC</button>
-            <button className="btn btn-lg btn-secondary" id="divide" >/</button>
-            <button className="btn btn-lg btn-secondary" id="add" >+</button>
-            <button className="btn btn-lg btn-secondary" id="subtract">-</button>
-            <button className="btn btn-lg btn-secondary" id="multiply">*</button>
+            <div className="display" id="display">{this.state.display}</div>  
+            <button className="btn btn-lg btn-danger" id="reset" onClick={this.reset} value="reset">AC</button>
+            <button className="btn btn-lg btn-secondary" id="divide" onClick={()=>this.handleClick("/")} >/</button>
+            <button className="btn btn-lg btn-secondary" id="add" onClick={()=>this.handleClick("+")} >+</button>
+            <button className="btn btn-lg btn-secondary" id="subtract"onClick={()=>this.handleClick("-")}>-</button>
+            <button className="btn btn-lg btn-secondary" id="multiply" onClick={()=>this.handleClick("*")}>*</button>
             {/* add shadow to buttons */}
-            <button className="btn button-secondary btn-lg" id='equal' onClick={this.lings}>=</button>
-            <button className="btn btn-primary btn-click" id='nine' value={9} onClick={this.lings}>9</button>
-            <button className="btn btn-primary btn-click" id='eight' onClick={this.lings}>8</button>
-            <button className="btn btn-primary btn-click" id='seven' onClick={this.lings}>7</button>
-            <button className="btn btn-primary btn-click" id='six' onClick={this.lings}>6</button>
-            <button className="btn btn-primary btn-click" id='five' onClick={this.lings}>5</button>
-            <button className="btn btn-primary btn-click" id='four' onClick={this.lings}>4</button>
-            <button className="btn btn-primary btn-click" id='three' onClick={this.lings}>3</button>
-            <button className="btn btn-primary btn-click" id='two' onClick={this.lings}>2</button>
-            <button className="btn btn-primary btn-click" id='one' onClick={this.lings}>1</button>
-            <button className="btn btn-primary btn-click" id='point' onClick={this.lings}>.</button>
-            <button className="btn btn-primary btn-click" id="zero" onClick={this.lings}>0</button>
+            <button className="btn button-secondary btn-lg" id='equal' onClick={this.equal}>=</button>
+            <button className="btn btn-primary btn-click" id='nine' onClick={()=>this.handleClick(9)}>9</button>
+            <button className="btn btn-primary btn-click" id='eight' onClick={()=>this.handleClick(8)}>8</button>
+            <button className="btn btn-primary btn-click" id='seven' onClick={()=>this.handleClick(7)}>7</button>
+            <button className="btn btn-primary btn-click" id='six' onClick={()=>this.handleClick(6)}>6</button>
+            <button className="btn btn-primary btn-click" id='five' onClick={()=>this.handleClick(5)}>5</button>
+            <button className="btn btn-primary btn-click" id='four' onClick={()=>this.handleClick(4)}>4</button>
+            <button className="btn btn-primary btn-click" id='three' onClick={()=>this.handleClick(3)}>3</button>
+            <button className="btn btn-primary btn-click" id='two' onClick={()=>this.handleClick(2)}>2</button>
+            <button className="btn btn-primary btn-click" id='one' onClick={()=>this.handleClick(1)}>1</button>
+            <button className="btn btn-primary btn-click" id='point' onClick={()=>this.handleClick(".")}>.</button>
+            <button className="btn btn-primary btn-click" id="zero" onClick={()=>this.handleClick(0)}>0</button>
         </div>
     )
  }
