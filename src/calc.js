e0dbee61989a060681a7699591cc81a52329f07d
KhayaZikhali <70526289+KhayaZikhali@users.noreply.github.com>
2021-02-12 20:10:15 +0100

    Added magnitude buttons
    fixed the error of numbers adding to each other rafter A/C button click

--- src/calc.js
+++ src/calc.js
@@ -1,6 +1,10 @@
-import React , { useState }from 'react'
+import React from 'react'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+document.addEventListener('keypress', ()=>{
+    console.log(KeyboardEvent.value)
+} )
 
 // find a way to display the numbers that a re clicked on to the display
 class Main extends React.Component {
@@ -9,30 +13,23 @@ class Main extends React.Component {
       this.state = {
        display  : " "
       };
+      //key bindings
       this.clear = this.clear.bind(this)
       this.handleClick = this.handleClick.bind(this)
+      this.thousand = this.thousand.bind(this)
       this.equal = this.equal.bind(this)
       this.handleKeyPress =this.handleKeyPress.bind(this)
     }
- //  add event listeners  
-    // handles when the button is clicked on using the cursor 
+    //Key board event listener
+
+    // handles when the button is clicked on using the cursor and console logs it           
     handleClick = (value) => {
         this.setState({ display : this.state.display + value})
-        console.log(this.state.display)
       }
-
     // handles when the button is clicked on using the cursor   
-    handleKeyPress = (key) => {
-        if(key === 1||2||3||4||5||6||7||8||9||0){
-            this.setState(state => {
-                return{display : key}
-            })
-        }
-    }
+    handleKeyPress = (key) => {}
 
-// remove the bug that occurs when the zero button is pressed
-
-// return the result of the 
+// return the result of the display content
       equal(){
           try {
             var result = document.getElementById('display').value;
@@ -41,16 +38,35 @@ class Main extends React.Component {
                 return{display : eval(this.state.display)}
             })
     }
+    // Catch the error if a non integer is entered 
         catch(err){ 
+            console.error();
             this.setState(state=>{
                 return{display : "NaN"}
             })
         }
-}
+}   
+       //** Handles Click eventfor the three accesorry buttons at the bottom of the screen 
+       thousand(){
+         if(this.state.display !== ""){
+         this.setState({ display : this.state.display + "000"})
+        }
+      }
+       million(){
+         this.state.display !=="" ? this.setState ({ display : this.state.display + "000000"});
+       }
+
+       billion(){
+         if(this.state.display !== ""){
+         this.setState({ display : this.state.display + "000000000"})
+        }
+      }
+
+      
     // clears the display and return zero   
     clear(){
         this.setState (state =>{ 
-            return {display : 0 }})
+            return {display : " " }})
     }
     
     render() {
@@ -63,22 +79,32 @@ class Main extends React.Component {
             <button className="btn btn-lg btn-secondary" id="subtract"onClick={()=>this.handleClick("-")} onKeyPress={() => this.handleKeyPress("-")} >-</button>
             <button className="btn btn-lg btn-secondary" id="multiply" onClick={()=>this.handleClick("*")} onKeyPress={() => this.handleKeyPress("*")} >*</button>
             <button className="btn button-secondary btn-lg" id='equals' onClick={this.equal} onKeyPress={() => this.handleKeyPress("=")}>=</button>
-            <button className="btn btn-primary btn-click" id='nine' onClick={()=>this.handleClick("9")} onKeyPress={()=>this.handleKeyPress(9)}>9</button>
-            <button className="btn btn-primary btn-click" id='eight' onClick={()=>this.handleClick(8)} onKeyPress={() => this.handleKeyPress(8)}>8</button>
-            <button className="btn btn-primary btn-click" id='seven' onClick={()=>this.handleClick(7)} onKeyPress={() => this.handleKeyPress(7)}>7</button>
-            <button className="btn btn-primary btn-click" id='six' onClick={()=>this.handleClick(6)} 
-            onKeyPress={() => this.handleKeyPress(6)} >6</button>
-            <button className="btn btn-primary btn-click" id='five' onClick={()=>this.handleClick(5)} onKeyPress={() => this.handleKeyPress(5)}>5</button>
-            <button className="btn btn-primary btn-click" id='four' onClick={()=>this.handleClick(4)} onKeyPress={() => this.handleKeyPress(4)} key={4}>4</button>
-            <button className="btn btn-primary btn-click" id='three' onClick={()=>this.handleClick(3)} onKeyPress={() => this.handleKeyPress(3)}>3</button>
-            <button className="btn btn-primary btn-click" id='two' onClick={()=>this.handleClick(2)}   onKeyPress={() => this.handleKeyPress(2)}>2</button>    
-            <button className="btn btn-primary btn-click" id='one' onClick={()=>this.handleClick(1)}onKeyPress={() => this.handleKeyPress(1)}>1</button>
+            <button className="btn btn-primary btn-click" id='nine'  onClick={()=>this.handleClick("9")} >9</button>
+            <button className="btn btn-primary btn-click" id='eight' onClick={()=>this.handleClick(8)} >8</button>
+            <button className="btn btn-primary btn-click" id='seven' onClick={()=>this.handleClick(7)} >7</button>
+            <button className="btn btn-primary btn-click" id='six'   onClick={()=>this.handleClick(6)} >6</button>
+            <button className="btn btn-primary btn-click" id='five'  onClick={()=>this.handleClick(5)} >5</button>
+            <button className="btn btn-primary btn-click" id='four' onClick={()=>this.handleClick(4)} >4</button>
+            <button className="btn btn-primary btn-click" id='three' onClick={()=>this.handleClick(3)} >3</button>
+            <button className="btn btn-primary btn-click" id='two' onClick={()=>this.handleClick(2)} >2</button>    
+            <button className="btn btn-primary btn-click" id='one' onClick={()=>this.handleClick(1)} >1</button>
             <button className="btn btn-primary btn-click" id='decimal' onClick={()=>this.handleClick(".")} 
-            onKeyPress={() => this.handleKeyPress(".")}>.</button>
-            <button className="btn btn-primary btn-click" id="zero" onClick={()=>this.handleClick("0")} onKeyPress={()=>this.handleKeyPress(5)}>0</button>
+            >.</button>
+            <button className="btn btn-primary btn-click" id="zero" onClick={()=>this.handleClick("0")} onKeydown={()=>this.handleKeyPress(0)}>0</button>
+            <button class="magnitude" id="thousand" onClick={this.thousand}> Thousand </button>
+            <button class="magnitude" onClick={this.million}>Million</button>
+            <button class="magnitude" onClick={this.billion} >Billion</button>
         </div>
     )
  }
 }
 
+
+// ! Add autocomplete support for the number 
+// ? i.e enable the user to click a button to add the neccesary number of zeros for billions
+// ? instead of having to manually type in all those zeros
+// ** COMPLETE THE SITE AND AT LEAST ONE PROJECT FROM YOUR LEARN LIST
+
+
+//work on a preview vid click 
 export default Main
